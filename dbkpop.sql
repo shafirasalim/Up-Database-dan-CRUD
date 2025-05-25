1dd7980dba10caaf34b8b47399aebe306e7cf15c
Siti Shafira Martiana Nurizqi Salim <shafira03excellent@gmail.com>
2025-05-25 21:32:47 +0700

    Membenarkan database

--- dbkpop.sql
+++ dbkpop.sql
@@ -15,3 +15,19 @@ INSERT INTO concerts (nama, lokasi, tanggal) VALUES
 ('NMIXX Debut Showcase', 'Bandung', '2022-12-01'),
 ('EXO Planet #5', 'Yogyakarta', '2019-09-20'),
 ('Red Velvet 4th Concert', 'Bali', '2023-11-25');
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    level INT NOT NULL CHECK (level IN (0, 1)) 
+);
+
+INSERT INTO users (username, password, level) VALUES
+('user01', '12345', 0),
+('admin01', '12345', 1);
+
+INSERT INTO users (username, password, level) VALUES
+('user01', '$2y$10$gtMOeaG8DQpvlyt5af44buvab8DY8UStvQkWEUEqVpgHvMfFi5wuO', 0),
+('admin01', '$2y$10$gtMOeaG8DQpvlyt5af44buvab8DY8UStvQkWEUEqVpgHvMfFi5wuO', 1);
+
